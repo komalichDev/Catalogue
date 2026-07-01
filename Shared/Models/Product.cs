3e1898b9562ea61f0ad8806ef8e3f35ee9006c08
komalichDev <liam.koch@messerli.ch>
2026-07-01 13:23:49 +0200

    DTO für den Client und das Backend.

---
+++ Shared/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public struct Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Description Description { get; set; }
+        public string Category { get; set; }
+
+    }
+}
