49bafe054a71f9dd94a211dc82082474565735b0
komalichDev <liam.koch@messerli.ch>
2026-07-30 16:10:09 +0200

    Anpassung und weiteres Modal

--- Client/Pages/CategorySearchSelection.razor.cs
+++ Client/Pages/CategorySearchSelection.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Client.Services;
+using Common.Types;
 using Microsoft.AspNetCore.Components;
 using Shared.Models;
 
@@ -7,27 +8,27 @@ namespace Client.Pages;
 public partial class CategorySearchSelection : BaseComponent
 {
     [Parameter]
-    public List<int> SelectedCategoryIds { get; set; } = new();
+    public List<CategoryId> SelectedCategoryIds { get; set; } = new();
 
     [Parameter]
-    public EventCallback<List<int>> OnConfigSaved { get; set; }
+    public EventCallback<List<CategoryId>> OnConfigSaved { get; set; }
 
     [Inject]
     private IHttpProductApi ProductApi { get; set; } = default!;
 
     private List<Category>? Categories { get; set; }
 
-    private List<int> TempSelectedIds { get; set; } = new();
+    private List<CategoryId> TempSelectedIds { get; set; } = new();
 
     private bool IsAllChecked
-        => Categories != null && Categories.Any() && Categories.All(c => TempSelectedIds.Contains(c.Id.Value));
+        => Categories != null && Categories.Any() && Categories.All(c => TempSelectedIds.Contains(c.Id));
 
     private bool IsIndeterminate
         => TempSelectedIds.Any() && !IsAllChecked;
 
     protected override async Task OnInitializedAsync()
     {
-        TempSelectedIds = new List<int>(SelectedCategoryIds);
+        TempSelectedIds = new List<CategoryId>(SelectedCategoryIds);
 
         var data = await ExecuteLoadAsync(() => ProductApi.LoadCategories());
         if (data != null)
@@ -41,7 +42,7 @@ public partial class CategorySearchSelection : BaseComponent
         bool isChecked = (bool)(e.Value ?? false);
         if (isChecked && Categories != null)
         {
-            TempSelectedIds = Categories.Select(c => c.Id.Value).ToList();
+            TempSelectedIds = Categories.Select(c => c.Id).ToList();
         }
         else
         {
@@ -49,7 +50,7 @@ public partial class CategorySearchSelection : BaseComponent
         }
     }
 
-    private void OnCategoryChanged(int id, ChangeEventArgs e)
+    private void OnCategoryChanged(CategoryId id, ChangeEventArgs e)
     {
         bool isChecked = (bool)(e.Value ?? false);
         if (isChecked && !TempSelectedIds.Contains(id))
---
+++ Client/Pages/RangeSearchSelection.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages;
+
+public partial class RangeSearchSelection : BaseComponent
+{
+    private double? _tempMin;
+    private double? _tempMax;
+
+    [Parameter]
+    public string Title { get; set; } = string.Empty;
+
+    [Parameter]
+    public string Unit { get; set; } = string.Empty;
+
+    [Parameter]
+    public double? MinValue { get; set; }
+
+    [Parameter]
+    public double? MaxValue { get; set; }
+
+    [Parameter]
+    public EventCallback<(double? Min, double? Max)> OnConfigSaved { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        _tempMin = MinValue;
+        _tempMax = MaxValue;
+    }
+
+    private async Task SaveConfig() 
+        => await OnConfigSaved.InvokeAsync((_tempMin, _tempMax));
+}
